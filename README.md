af91c4edaeee31db02ce7382a64f5fccfe5c67ba
cenfun <cenfun@gmail.com>
2021-03-16 21:44:51 +0800

    add 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # ie-polyfill
-ie-polyfill
+> The way for ie to continue working
+
+##  Why this?
+* [@babel/polyfill](https://babel.dev/docs/en/babel-polyfill) has been deprecated since Babel 7.4.0
+* want to load latest core-js and regenerator-runtime without build-ins
+## Versions
+* [core-js/stable](https://github.com/zloirock/core-js) ^3.9.1
+* [regenerator-runtime](https://github.com/facebook/regenerator/tree/master/packages/regenerator-runtime) ^0.13.7
+
+
+## Install
+```sh
+npm i ie-polyfill
+```
+
+## Usage
+```js
+import "ie-polyfill"
+//or
+require("ie-polyfill")
+```
+
+## In Browser
+```html
+<script src="path-to/ie-polyfill/dist/ie-polyfill.js"></script>
+```
+
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const StatsReportPlugin = require("webpack-stats-report").StatsReportPlugin;
+
+module.exports = {
+    mode: "production",
+    cache: true,
+    target: ["web", "es5"],
+    devtool: "source-map",
+    output: {
+        filename: "ie-polyfill.js",
+        umdNamedDefine: true,
+        library: "ie-polyfill",
+        libraryTarget: "umd"
+    },
+    plugins: [new StatsReportPlugin({
+        title: "Stats Report - ie-polyfill",
+        output: ".temp/stats-report.html",
+        outputStatsJson: true,
+        generateMinifiedAndGzipSize: true
+    })],
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    cacheDirectory: true,
+                    babelrc: false,
+                    presets: ["@babel/preset-env"]
+                }
+            }
+        }]
+    }
+};
